12e2e65e72d1d4832817f2d48b53d2b4c100c22c
Tyson Hester <ironman9967@gmail.com>
2015-06-30 23:48:56 -0400

    refactored for twitch (we changed our minds :)

--- dead-worker.js
+++ dead-worker.js
@@ -1,17 +1,14 @@
 ///<reference path='./node_modules/ironworks/ironworks.d.ts' />
+///<reference path='./typings/request/request.d.ts' />
 var __extends = this.__extends || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
     __.prototype = b.prototype;
     d.prototype = new __();
 };
+var _ = require('lodash');
 var ironworks = require('ironworks');
-var deadType;
-(function (deadType) {
-    deadType[deadType["Celeb"] = 0] = "Celeb";
-    deadType[deadType["Musician"] = 1] = "Musician";
-    deadType[deadType["Sports"] = 2] = "Sports";
-})(deadType || (deadType = {}));
+var request = require('request');
 var DeadWorker = (function (_super) {
     __extends(DeadWorker, _super);
     function DeadWorker(opts) {
@@ -21,24 +18,77 @@ var DeadWorker = (function (_super) {
             id: ironworks.helpers.idHelper.newId(),
             name: 'dead-worker'
         });
-        var defOpts = {};
+        var defOpts = {
+            max: 300,
+            updateFreq: 60 * 60 * 1000
+        };
         this.opts = this.opts.beAdoptedBy(defOpts, 'worker');
         this.opts.merge(opts);
+        this.streams = [];
     }
     DeadWorker.prototype.init = function (comm, whoService, cb) {
         this.setComm(comm, whoService);
-        this.respond('dead', function (req, cb) {
-            cb(null, {
-                name: 'test',
-                pictureUrl: 'n/a',
-                details: {
-                    cod: 'unknown',
-                    timestamp: new Date().getTime(),
-                    location: 'unknown'
+        var instance = this;
+        DeadWorker.download(this.opts.get('max'), function (streams) {
+            instance.streams = streams;
+            instance.respond('dead', function (req, respond) {
+                if (_.isUndefined(req.offset)) {
+                    req.offset = 0;
+                }
+                if (_.isUndefined(req.offset)) {
+                    req.limit = 5;
+                }
+                if (req.offset > instance.streams.length) {
+                    respond(new Error("only have " + instance.streams.length + " streams."));
+                }
+                else {
+                    respond(null, _.take(instance.streams.slice(req.offset), req.limit));
                 }
             });
+            cb(null);
         });
-        cb(null);
+    };
+    DeadWorker.prototype.update = function () {
+        var max = this.opts.get('max');
+        var updateFreq = this.opts.get('updateFreq');
+        var instance = this;
+        DeadWorker.download(max, function (streams) {
+            instance.streams = streams;
+            instance.updateTimeout = setTimeout(function () {
+                instance.update();
+            }, updateFreq);
+        });
+    };
+    DeadWorker.download = function (max, cb, streams) {
+        if (_.isUndefined(streams)) {
+            streams = [];
+        }
+        var offset = streams.length;
+        request.get('https://api.twitch.tv/kraken/streams?limit=100&' + offset, function (e, res) {
+            var current = JSON.parse(res.body);
+            _.each(current.streams, function (s) {
+                streams.push({
+                    id: s._id,
+                    game: s.game,
+                    viewers: s.viewers,
+                    preview: s.preview.template,
+                    displayName: s.channel.display_name,
+                    logo: s.channel.logo,
+                    statusMessage: s.channel.status,
+                    url: s.channel.url,
+                    followers: s.channel.followers,
+                    views: s.channel.views
+                });
+            });
+            if (streams.length < max) {
+                DeadWorker.download(max, function (s) {
+                    cb(s);
+                }, streams);
+            }
+            else {
+                cb(streams);
+            }
+        });
     };
     return DeadWorker;
 })(ironworks.workers.Worker);
--- dead-worker.ts
+++ dead-worker.ts
@@ -1,29 +1,39 @@
 
 ///<reference path='./node_modules/ironworks/ironworks.d.ts' />
+///<reference path='./typings/request/request.d.ts' />
+
+import _ = require('lodash');
 
 import ironworks = require('ironworks');
+import request = require('request');
 
-enum deadType {
-    Celeb,
-    Musician,
-    Sports
+interface IStreamRequest {
+    limit: number;
+    offset?: number;
 }
 
-interface IDetails {
-    cod: string;
-    timestamp: number;
-    location: string;
+interface IStream {
+    id: number;
+    game: string;
+    viewers: number;
+    preview: string;
+    displayName: string;
+    logo: string;
+    statusMessage: string;
+    url: string;
+    followers: number;
+    views: number;
 }
 
-interface IPersonEntry {
-    name: string;
-    pictureUrl: string;
-    details: IDetails;
+interface IDeadWorkerOpts {
+    max: number;
+    updateFreq: number;
 }
-
-interface IDeadWorkerOpts {}
 
 class DeadWorker extends ironworks.workers.Worker {
+    private updateTimeout: number;
+    private streams: IStream[];
+
     constructor(opts?: IDeadWorkerOpts) {
         super([
             'iw-socket'
@@ -32,25 +42,81 @@ class DeadWorker extends ironworks.workers.Worker {
             name: 'dead-worker'
         });
 
-        var defOpts: IDeadWorkerOpts = { };
+        var defOpts: IDeadWorkerOpts = {
+            max: 300,
+            updateFreq: 60 * 60 * 1000
+        };
         this.opts = this.opts.beAdoptedBy<IDeadWorkerOpts>(defOpts, 'worker');
         this.opts.merge(opts);
+
+        this.streams = [];
     }
 
     public init(comm, whoService, cb) {
         this.setComm(comm, whoService);
-        this.respond<deadType, IPersonEntry>('dead', (req, cb) => {
-            cb(null, {
-                name: 'test',
-                pictureUrl: 'n/a',
-                details: {
-                    cod: 'unknown',
-                    timestamp: new Date().getTime(),
-                    location: 'unknown'
+        var instance = this;
+        DeadWorker.download(this.opts.get<number>('max'), (streams) => {
+            instance.streams = streams;
+            instance.respond<IStreamRequest, IStream[]>('dead', (req, respond) => {
+                if (_.isUndefined(req.offset)) {
+                    req.offset = 0;
+                }
+                if (_.isUndefined(req.offset)) {
+                    req.limit = 5;
+                }
+                if (req.offset > instance.streams.length) {
+                    respond(new Error("only have " + instance.streams.length + " streams."));
+                }
+                else {
+                    respond(null, _.take(instance.streams.slice(req.offset), req.limit));
                 }
             });
+            cb(null);
         });
-        cb(null);
+    }
+
+    private update() {
+        var max = this.opts.get<number>('max');
+        var updateFreq = this.opts.get('updateFreq');
+        var instance = this;
+        DeadWorker.download(max, (streams) => {
+            instance.streams = streams;
+            instance.updateTimeout = setTimeout(() => {
+                instance.update();
+            }, updateFreq);
+        });
+    }
+
+    private static download(max: number, cb: (streams: any[]) => void, streams?: IStream[]) {
+        if (_.isUndefined(streams)) {
+            streams = [];
+        }
+        var offset = streams.length;
+        request.get('https://api.twitch.tv/kraken/streams?limit=100&' + offset, (e, res: any) => {
+            var current = JSON.parse(res.body);
+            _.each(current.streams, (s: any) => {
+                streams.push({
+                    id: s._id,
+                    game: s.game,
+                    viewers: s.viewers,
+                    preview: s.preview.template,
+                    displayName: s.channel.display_name,
+                    logo: s.channel.logo,
+                    statusMessage: s.channel.status,
+                    url: s.channel.url,
+                    followers: s.channel.followers,
+                    views: s.channel.views
+                });
+            });
+            if (streams.length < max) {
+                DeadWorker.download(max, (s) => {
+                    cb(s)
+                }, streams);
+            }
+            else {
+                cb(streams);
+            }
+        });
     }
 }
 
